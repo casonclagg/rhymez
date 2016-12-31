fc9baf294281f2d13b744dbfc0b783742860925c
Cason Clagg <casonclagg@gmail.com>
2016-12-30 22:53:13 -0600

    added a test

--- test/build-tests.js
+++ test/build-tests.js
@@ -1,13 +1,21 @@
-import Rhymez from '../src/rhymez';
+import Rhymez from '../lib/rhymez';
 import _ from 'lodash'
 import {assert} from 'chai'
 
-suite('Speed:', () => {
+suite('build:', () => {
+    test('lib can rhyme', () => {
+        let r = new Rhymez()
+        r.load().then(() => {
+            let rhymes = r.rhyme("dog", {isLoose: false})
+
+            assert.isTrue(_.includes(rhymes, "LOG"))
+        })
+    })
 
     // test('rhymez is reasonably fast', async () => {
     //     let COUNT = 100
     //     let thingsToRhyme = "this is great, guys.".split(" ")
-	// 	let r = new Rhymez()
+    // 	let r = new Rhymez()
     //     await r.load()
     //     let start = Date.now()
     //     for(var i = 0; i < COUNT; i++) {
@@ -16,6 +24,6 @@ suite('Speed:', () => {
     //     let end = Date.now()
     //     console.log((end - start) / COUNT, "ms each call")
     //     assert.isBelow(end - start, COUNT * 100)
-	// })
+    // })
 
 })
